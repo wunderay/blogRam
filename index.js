7df8e69f1d3cf4ad1fa0767ef812b7dc067dfead
Wunderay <60483832+wunderay@users.noreply.github.com>
2024-08-21 13:37:34 -0700

    feat: table of contents and viewing posts

--- index.js
+++ index.js
@@ -7,8 +7,36 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }) );
 app.use(express.json());
 
+class Post {
+  postId;
+  title;
+  author;
+  content;
+
+  constructor(title, author, content) {
+    this.title = title;
+    this.author = author;
+    this.content = content;
+    this.postId = Date.now().toString(36) + Math.random().toString(36).substring(2, 12).padStart(12, 0);
+  }
+}
+
+const blogPosts = [];
+
+const blogIndex = [];
+
+class postCatalogue {
+  title;
+  url;
+
+  constructor(title, postId) {
+    this.title = title;
+    this.url = postId;
+  }
+}
+
 app.get("/", (req, res) => {
-    res.render("main.ejs");
+    res.render("main.ejs", {blogIndex});
   });
 
 app.get("/write", (req, res) => {
@@ -16,7 +44,22 @@ app.get("/write", (req, res) => {
   });
 
   app.post("/write", (req, res) => {
-    res.render("main.ejs");
+    let newPost = new Post(req.body.title, req.body.author, req.body.content);
+    blogPosts.push(newPost);
+    let newPostCatalogue = new postCatalogue(newPost.title, newPost.postId);
+    blogIndex.push(newPostCatalogue);
+    res.render("main.ejs", {blogIndex});
+  });
+
+  app.get("/:id", (req, res) => {
+    let params = req.params;
+    console.log(params);
+    let blogPost = blogPosts.find((post) => post.postId == params.id);
+    if (blogPost == undefined) {
+      res.render("main.ejs", {blogIndex});
+    } else {
+      res.render("post.ejs", {blogPost});
+    }
   });
 
 app.listen(port, () => {
