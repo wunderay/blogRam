a01b0c6b69cb1bf640f7918be0c95f8fa8fba34e
Wunderay <60483832+wunderay@users.noreply.github.com>
2024-08-20 20:48:53 -0700

    feat: add header, footer, post, and write scaffolding

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get("/", (req, res) => {
     res.render("main.ejs");
   });
 
+app.get("/write", (req, res) => {
+    res.render("write.ejs");
+  });
+
+  app.post("/write", (req, res) => {
+    res.render("main.ejs");
+  });
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
